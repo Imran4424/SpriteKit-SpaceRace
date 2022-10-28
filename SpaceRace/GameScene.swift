35958d12b84a26012b4be9206b4e3666599c8958
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-10-29 03:05:41 +0600

    create enemy

--- SpaceRace/GameScene.swift
+++ SpaceRace/GameScene.swift
@@ -13,6 +13,10 @@ class GameScene: SKScene {
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
     
+    var possibleEnemies = ["ball", "hammer", "tv"]
+    var gameTimer: Timer!
+    var isGameOver = false
+    
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
@@ -44,10 +48,45 @@ class GameScene: SKScene {
         
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+    
+    @objc func createEnemy() {
+        guard let enemy = possibleEnemies.randomElement() else {
+            print("failed to select random element")
+            return
+        }
+        
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        // will spin at constant speed
+        sprite.physicsBody?.angularVelocity = 5
+        // linear dumping defines, how fast a object slow down over time
+        // 0 means never
+        // linear friction
+        sprite.physicsBody?.linearDamping = 0
+        // will never stop spinning
+        // angular friction
+        sprite.physicsBody?.angularDamping = 0
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
         
+        if !isGameOver {
+            score += 1
+        }
     }
 }
 
